7c82419799eabb80348425e7ca9f67907a4a579b
yegan <yeganj@gmail.com>
2019-05-17 12:54:13 +0200

    token class test1 complete

--- src/main/java/TLL/Tokens.java
+++ src/main/java/TLL/Tokens.java
@@ -1,8 +1,29 @@
 package TLL;
 
-public class Tokens {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-    public Tokens(){
+public class Tokens {
+    Learner learner;
+    HashMap<String,Integer> studentTokenMap = new HashMap<>();
+    HashMap<String,Integer> teacherTokenMap = new HashMap<>();
+
+
+    public void getStudentLesson(Lesson lesson){
+        try{
+            ArrayList<String> students = lesson.studentsAttendingLesson;
+            for (int i =0; i<students.size();i++){
+             studentTokenMap.put(students.get(i),3);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
 
     }
+
+    public HashMap studentsTokenTotalMap(){
+        return studentTokenMap;
+    }
+
 }
